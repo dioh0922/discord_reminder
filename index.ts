bfb893d757b3bcbf9d775b75b62f166777d9b050
dioh0922 <diohound@gmail.com>
2026-06-11 12:20:11 +0900

    サーバ管理バッチ確認

--- index.ts
+++ index.ts
@@ -8,6 +8,8 @@ import {
 } from 'discord.js';
 import { db } from './src/db.js';
 import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import { DateTime } from 'luxon';
 // .env ファイルから環境変数を読み込み
 dotenv.config();
 
@@ -22,6 +24,10 @@ const selectMenu = new StringSelectMenuBuilder()
     {
       label: '映画',
       value: 'movie',
+    },
+    {
+      label: 'サーバー確認',
+      value: 'svr',
     },
   ]);
 
@@ -48,7 +54,6 @@ client.once(Events.ClientReady, async (readyClient) => {
       const channel = await readyClient.channels.fetch(channelId);
       if (channel && 'send' in channel) {
         channel.send({ content: 'Botが起動しました 🤖', components: [row] });
-        //await channel.send('Botが起動しました 🤖');
       }
     } catch (error) {
       console.error('起動メッセージの送信に失敗しました:', error);
@@ -62,15 +67,47 @@ client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isStringSelectMenu()) return;
   if (interaction.customId === 'menu') {
     const selectedValue = interaction.values[0];
+    let content = '';
     if (selectedValue === 'movie') {
       const movieData = await db.selectFrom('moviedata').selectAll().orderBy('date', 'desc').limit(5).execute();
-      console.log(movieData);
-      await interaction.reply({
-        content: `【映画】最後に見た5つは\n${movieData.map((movie) => movie.title).join('\n')}`
-      });
+      content = `【映画】最後に見た5つは\n${movieData.map((movie) => movie.title).join('\n')}`;
+    }else if(selectedValue === 'svr'){
+      const logPath = process.env.BATCH_LOG_DIR;
+      const certLog = logPath + "/cert.log";
+      const mydnsLog = logPath + "/notice_mydns.jp";
+      const datePattern = '/([A-Za-z]{3} \s*\d{1,2} \s*\d{2}:\d{2}:\d{2} \s*[APM]{2} \s*[A-Z]{3} \s*\d{4})/';
+      let latestDate = null;
+      if(fs.existsSync(certLog)){
+        
+        const logStr = fs.readFileSync(certLog, 'utf8');
+        const matches = logStr.match(datePattern);
+        if(matches){
+          const dateTime = DateTime.fromFormat(
+            matches[0],
+            'yyyy/MM/dd HH:mm:ss ZZZ'
+          );
+          latestDate = dateTime.toFormat('Y/m/d');
+        }
+      }
+      if(fs.existsSync(mydnsLog)){
+        const logStr = fs.readFileSync(mydnsLog, 'utf8');
+        const matches = logStr.match(datePattern);
+        if(matches){
+          const dateTime = DateTime.fromFormat(
+            matches[0],
+            'yyyy/MM/dd HH:mm:ss ZZZ'
+          );
+          latestDate = dateTime.toFormat('Y/m/d');
+        }
+      }
+      content = `【サーバー確認】certbot最新：${latestDate}\nmydns最新：${latestDate}`;
     }else{
-      await interaction.reply({ content: '選択された値: ' + selectedValue, components: [row] });
+      content = '選択された値: ' + selectedValue;
     }
+    await interaction.reply({ 
+      content: content,
+      components: [row],
+    });
   }
 });
 
